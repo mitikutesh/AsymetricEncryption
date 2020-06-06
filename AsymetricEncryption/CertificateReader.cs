112a5d15cf60a9b73a535ab3e47f526267796c19
Mitiku Teshome <mitikutesh@gmail.com>
2020-06-06 16:34:49 +0300

    -done

---
+++ AsymetricEncryption/CertificateReader.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AsymetricEncryption
+{
+    public class CertificateReader
+    {
+       
+        public static X509Certificate2 selectCert(string thumbPrint)
+        {
+
+            var store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            X509Certificate2 certificate = new X509Certificate2 ();
+            store.Open(OpenFlags.ReadOnly);
+            //In real life here we fetch with thumprint or by name
+            var cert = store.Certificates.OfType<X509Certificate2>().FirstOrDefault();
+
+            return cert;
+        }
+    }
+}
---
+++ AsymetricEncryption/Encrypt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace AsymetricEncryption
+{
+    public class Encrypt
+    {
+        
+        public static byte[] EncryptDataOaepSha1(X509Certificate2 cert, byte[] data)
+        {
+            // GetRSAPublicKey returns an object with an independent lifetime, so it should be
+            // handled via a using statement.
+            using (RSA rsa = cert.GetRSAPublicKey())
+            {
+                // OAEP allows for multiple hashing algorithms, what was formermly just "OAEP" is
+                // now OAEP-SHA1.
+                return rsa.Encrypt(data, RSAEncryptionPadding.OaepSHA512);
+            }
+        }
+        public static byte[] DecryptDataOaepSha1(X509Certificate2 cert, byte[] data)
+        {
+            // GetRSAPrivateKey returns an object with an independent lifetime, so it should be
+            // handled via a using statement.
+            using (RSA rsa = cert.GetRSAPrivateKey())
+            {
+                return rsa.Decrypt(data, RSAEncryptionPadding.OaepSHA512);
+            }
+        }
+    }
+}
